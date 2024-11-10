247f8d77d4de8eedb1a780b92f83761801e57577
Audrine-m <audrinemarion@zetech.ac.ke>
2024-11-10 19:49:01 +0300

    commit changes

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ fetch('login.html')
     loginContainer.innerHTML = data;
   })
   .catch(error => console.error(error));
+
+  const footer = document.getElementById('footer');
+
+  footer.innerHTML = 'Loading footer...'; // Add a loading message
+  
+  fetch('footer.html')
+    .then(response => response.text())
+    .then(data => {
+      footer.innerHTML = data;
+    })
+    .catch(error => {
+      footer.innerHTML = 'Error loading footer.';
+      console.error(error);
+    });
